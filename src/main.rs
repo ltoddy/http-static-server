8db409d65746a78dd152e532f43d4f8888066ab5
ltoddy <tao.liu1@thoughtworks.com>
2019-11-25 21:41:16 +0800

    listener bind ip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use async_std::net::TcpListener;
-use async_std::net::ToSocketAddrs;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
 use async_std::task;
+use log::error;
 use log::info;
+use log::Level;
 use structopt::StructOpt;
 
 use error::Result;
+use std::future::Future;
 
 mod error;
 
@@ -18,17 +22,49 @@ struct Config {
 }
 
 fn main() {
+    let default_ip = String::from("127.0.0.1");
+    let default_port = String::from("8080");
+
     let config: Config = Config::from_args();
-    simple_logger::init().unwrap();
+    simple_logger::init_with_level(Level::Info).unwrap();
     info!("Starting the server ......");
 
-    let ip = config.ip.unwrap_or(String::from("127.0.0.1"));
-    let port = config.port.unwrap_or(String::from("8080"));
+    let ip = config.ip.unwrap_or(default_ip);
+    let port = config.port.unwrap_or(default_port);
 
-    task::block_on(server(format!("{}:{}", ip, port)));
+    task::block_on(serve(ip, port));
 }
 
-async fn server(addr: impl ToSocketAddrs) -> Result<()> {
-    let listener = TcpListener::bind(addr).await?;
+async fn serve(ip: String, port: String) -> Result<()> {
+    let addrs = format!("{}:{}", ip, port);
+    let listener = TcpListener::bind(addrs).await?;
+    info!("Server run on: {}:{}", ip, port);
+
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        info!("Accept from: {}", stream.peer_addr()?);
+        spawn_and_log_error(accept_connection(stream));
+    }
+
+    Ok(())
+}
+
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+    where
+        F: Future<Output=Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e)
+        }
+    })
+}
+
+async fn accept_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buffer = [0u8; 4096];
+
+    stream.read(&mut buffer).await?;
+
     Ok(())
 }
